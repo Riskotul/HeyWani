82011112a4992e03e3e6650ec0af2d7915c7e402
riskotulmaula <maulariskotul@gmail.com>
2024-05-23 22:03:13 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    theme: {
+      extend: {
+        fontFamily: {
+          poppins: ['Poppins', 'sans-serif'],
+        },
+        colors: {
+          orange: {
+            500: '#FA9F42',
+          },
+          green: {
+            400: '#8DD67A',
+          },
+        },
+        zIndex: {
+          '1000': '1000',
+        },
+      },
+    },
+    plugins: [],
+}
+  
+
+
